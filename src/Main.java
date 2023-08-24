7ef1859f5b6d4c05a34b6f403cae8eaffd4566b4
IsacHedenskog <isac.hedenskog@gmail.com>
2023-08-24 12:20:32 +0200

    hur input och output funkar

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+/*
+Todo: Hur ska hänga gubbe fungera?
+    * Ha ett ord
+     - Statiskt?
+     - Random från lista?
+     - Input
+    * Användare matar in bokstav
+     - kolla att användaren matar in en bokstav
+    * Kolla om bokstaven finns i ordet
+     - finns det flera instanser
+    * Kolla vart bokstaven är
+     - skriv ut bokstav
+    * Ha koll på liv
+    * Loopa tills klart
+     - slut på liv
+     - tills rätt ord
+
+ */
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hej och välkommen till hänga gubbe!");
+        // howInputAndOutputWorks();
+
+    }
+
+    public static void howInputAndOutputWorks(){
+        Scanner myScan= new Scanner(System.in);
+        String input= myScan.nextLine();
+
+        System.out.println("Min input var " + input);
     }
 }
+
+
+// single comment
+/*
+multiple
+comments
+ */
